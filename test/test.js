b844fab8f8a2f7ca131826cab22c6a2100dab1e1
Christoph Witzko <github@christophwitzko.com>
2015-01-27 12:56:58 +0100

    feat: added missing functions

--- test/test.js
+++ test/test.js
@@ -7,12 +7,27 @@ dotenv.load()
 
 var service = aaas({server: 'https://aaas-test.herokuapp.com/', username: process.env.USERNAME, password: process.env.PASSWORD})
 
-service.clearStore(function(err){
+service.newArray(function(err, arr){
   if(err) return console.log(err)
-  console.log('cleared')
+  arr.unshift('A', function(err){
+    if(err) return console.log(err)
+    arr.remove(0, function(err, data){
+      if(err) return console.log(err)
+      console.log(data)
+      arr.removeArray(function(err){
+        if(err) return console.log(err)
+        console.log('array removed')
+      })
+    })
+  })
 })
 
-service.getStoreKeys(function(err, data){
+/*service.clearStore(function(err){
+  if(err) return console.log(err)
+  console.log('cleared')
+})*/
+
+/*service.getStoreKeys(function(err, data){
   if(err) return console.log(err)
   console.log(data.keys)
-})
+})*/
